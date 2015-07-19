7842e29f2265a2ef5abfe0cacb4cbf88687b171c
tfennelly <tom.fennelly@gmail.com>
2015-07-19 21:25:37 +0100

    jqueryui to use jqueryui-detached/decorator

--- jquery-detached/js/jqueryui1.js
+++ jquery-detached/js/jqueryui1.js
@@ -1,8 +1,8 @@
 var jquery = require('jquery-detached-2.1.4');
 var $ui = jquery.newJQuery();
-var jqueryUIFactory = require('jqueryui-detached-1.11');
+var decorator = require('jqueryui-detached-1.11/decorator');
 
-jqueryUIFactory.addToJQuery($ui);
+decorator.addToJQuery($ui);
 
 exports.getJQueryUI = function() {
     return $ui;
