81e7b8078e62ac7d5061f997acc890e93eb2c5fe
tfennelly <tom.fennelly@gmail.com>
2015-09-08 13:46:30 +0100

    jenkins-handlebars-rt version to 1.0.1

--- handlebars/runtimes/handlebars3_rt.js
+++ handlebars/runtimes/handlebars3_rt.js
@@ -3,7 +3,7 @@
 var handlebars = {};
 
 require('jenkins-js-modules').import('handlebars:handlebars3')
-    .then(function(handlebars3) {
+    .onFulfilled(function(handlebars3) {
         for (var member in handlebars3) {
             handlebars[member] = handlebars3[member];
         }
