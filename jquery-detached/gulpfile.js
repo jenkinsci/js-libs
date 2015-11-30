3c58333937191d2c6313f44c1f21da29142a0c18
tfennelly <tom.fennelly@gmail.com>
2015-11-30 09:31:06 +0000

    Removed some boilerplate stuff from gulpfiles

--- jquery-detached/gulpfile.js
+++ jquery-detached/gulpfile.js
@@ -1,11 +1,6 @@
 var builder = require('jenkins-js-builder');
 
 //
-// Use the predefined tasks from jenkins-js-builder.
-//
-builder.defineTasks(['test', 'bundle']);
-
-//
 // Bundle the modules.
 //
 builder.bundle('js/jquery2.js')
--- momentjs/gulpfile.js
+++ momentjs/gulpfile.js
@@ -1,11 +1,6 @@
 var builder = require('jenkins-js-builder');
 
 //
-// Use the predefined tasks from jenkins-js-builder.
-//
-builder.defineTasks(['test', 'bundle']);
-
-//
 // Bundle the module.
 //
 builder.bundle('moment', 'momentjs2')
