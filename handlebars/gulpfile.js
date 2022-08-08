16d06afacc76e0b352e431f342d0aedf1a9791c8
Thorsten Scherler <scherler@gmail.com>
2022-08-08 19:16:40 +0200

    chore: [feature/handlebar4] publish to omething else
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- handlebars/gulpfile.js
+++ handlebars/gulpfile.js
@@ -3,10 +3,6 @@ const builder = require('jenkins-js-builder');
 //
 // Bundle the module.
 //
-builder.bundle('handlebars', 'handlebars3')
-    .asJenkinsModuleResource()
-    .export();
-//
 builder.bundle('handlebars', 'handlebars4')
     .asJenkinsModuleResource()
     .export();
